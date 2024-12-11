e07c1296a247c4fad6737e45e559445051e68942
sanchir94 <sancir9494@gmail.com>
2024-12-11 18:13:44 +0800

    ehlel

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="w-[1850px] h-[1500px] flex justify-center items-center">
         <div className="w-[800px] h-[1200px] bg-[#F3F4F6]"></div>
         <div className="w-[800px] h-[1200px] bg-[#0f141e]"></div>
-        <div className="w-[100px] h-[100px] rounded-full bg-white  absolute"></div>
+        <div className="w-[100px] h-[100px] rounded-full bg-white    absolute"></div>
       </div>
     </div>
   );
